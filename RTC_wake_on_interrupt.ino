d05a301f4c5df0d4cb58a9c1cbd931f42466cefa
John Franco Saraceno <OneGneissGuy@users.noreply.github.com>
2017-07-18 16:28:27 -0700

    Light refactoring of serial reporting

--- RTC_wake_on_interrupt.ino
+++ RTC_wake_on_interrupt.ino
@@ -14,9 +14,6 @@ RTC_DS3231 RTC;      //we are using the DS3231 RTC
 volatile uint16_t interuptCount = 0;
 volatile bool interuptFlag = false;
 byte i;
-byte AlarmFlag = 0;
-byte ledStatus = 1;
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 char dateBuffer[12];
 //-------------------------------------------------
 
@@ -36,6 +33,33 @@ void clear_alarms(){
   RTC.alarmInterrupt(2, false);
 }
 
+void print_wake_info(DateTime now){
+    //print the time that when we have woken up
+    Serial.print("Woken up from sleep at ");    
+    sprintf(dateBuffer,"%02u-%02u-%04u ",now.month(),now.day(),now.year());
+    Serial.print(dateBuffer);
+    sprintf(dateBuffer,"%02u:%02u:%02u ",now.hour(),now.minute(),now.second());
+    Serial.println(dateBuffer);
+    Serial.print("Awoken ");
+    Serial.print(interuptCount);
+    Serial.println(" time(s) since program was compiled");    
+    //Print that we are going back to sleep in the following loop
+    Serial.println("Going back to sleep...");
+}
+
+void setup_alarm(){
+   //clear any pending alarms
+  clear_alarms();  
+  //Set SQW pin to OFF (in my case it was set by default to 1Hz)
+  //The output of the DS3231 INT pin is connected to this pin
+  //It must be connected to arduino D2 pin for wake-up
+  RTC.writeSqwPinMode(DS3231_OFF);
+  //Set alarm2 every minute
+  RTC.setAlarm( ALM1_MATCH_SECONDS, 0, 0, 0, 0 ); // alarm1 flag will be set every minute
+  RTC.alarmInterrupt(1, true); //enable alarm 2
+  attachInterrupt(digitalPinToInterrupt(interruptPin), an_isr, FALLING);//attach isr to alarm interupt
+}
+
 void setup() {
   Serial.begin(9600);
   Serial.println("Starting wakeup alarm/timer sketch");
@@ -46,19 +70,7 @@ void setup() {
   //Initialize communication with the clock
   Wire.begin();
   RTC.begin();
-  //clear any pending alarms
-  clear_alarms();
-  
-  //Set SQW pin to OFF (in my case it was set by default to 1Hz)
-  //The output of the DS3231 INT pin is connected to this pin
-  //It must be connected to arduino D2 pin for wake-up
-  RTC.writeSqwPinMode(DS3231_OFF);
-
-  //Set alarm2 every minute
-  RTC.setAlarm( ALM1_MATCH_SECONDS, 0, 0, 0, 0 ); // alarm1 flag will be set every minute
-
-  RTC.alarmInterrupt(1, true); //enable alarm 2
-  attachInterrupt(digitalPinToInterrupt(interruptPin), an_isr, FALLING);//attach isr to alarm interupt
+  setup_alarm(); 
 }
 
 //------------------------------------------------------------
@@ -73,25 +85,18 @@ void loop()
     detachInterrupt(interruptPin); //detach the interupt
   
     DateTime now = RTC.now(); //get the current time 
-    clear_alarms();//clear previous RTC alarms
-    //blink the LED to show that we have woken up
+    print_wake_info(now);
+      //blink the LED to show that we have woken up
     for (i = 0; i < 11; i++){      
       digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
       delay(100);                   // wait for a while
       digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
       delay(100);                   // wait for a while
     }
+    //clear previous RTC alarms
+    clear_alarms();
     //setup the alarms again
     RTC.setAlarm( ALM1_MATCH_SECONDS, 0, 0, 0, 0 ); // alarm1 flag will be set every minute
     RTC.alarmInterrupt(1, true); //enable alarm 2
-    //print the time that when we have woken up
-    Serial.print("Woken up from sleep at ");    
-    sprintf(dateBuffer,"%02u-%02u-%04u ",now.month(),now.day(),now.year());
-    Serial.print(dateBuffer);
-    sprintf(dateBuffer,"%02u:%02u:%02u ",now.hour(),now.minute(),now.second());
-    Serial.println(dateBuffer);
-    //Print that we are going back to sleep in the following loop
-    Serial.println("Going back to sleep...");
-    delay(100);
-  
+    delay(100);  
 }
